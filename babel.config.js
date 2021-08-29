1576c3a859cba6d03382aad119c57d4156068a38
Ismael Albert <albertismael7@gmail.com>
2021-08-29 12:57:49 -0300

    typescript

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    /* 1° part: */
-    presets: [
-        '@babel/preset-env',
-        /*configuração pro preset importar o react nos arquivos */
-        ['@babel/preset-react', { runtime: 'automatic' }]
-    ]
-}
+  /* 1° part: */
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    /*configuração pro preset importar o react nos arquivos */
+    ["@babel/preset-react", { runtime: "automatic" }],
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,55 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
-const isDevelopment = process.env.NODE_ENV !== 'production';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production',
+  mode: isDevelopment ? "development" : "production",
 
-    /*ource maps para que seja possível debugar o código escrito da aplicação durante o desenvolvimento mesmo com o bundle deixando o nosso código confuso. */
-    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  /*ource maps para que seja possível debugar o código escrito da aplicação durante o desenvolvimento mesmo com o bundle deixando o nosso código confuso. */
+  devtool: isDevelopment ? "eval-source-map" : "source-map",
 
-    /*entry -> fala onde fica o arquivo principal */
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    devServer: {
-        static: path.resolve(__dirname, 'public'),
-        hot: true,
-    },
-    plugins: [
-        isDevelopment && new ReactRefreshWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        })
-    ].filter(Boolean),
-    /*como a aplicação vai se comportar */
-    module: {
-        rules:[
-            {
-                /*test recebe uma expressão regular pra dizer se o arquivo é js ou não */
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                /*babel-loade faz a integração do babel e webapck */
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        plugins:[
-                            isDevelopment && require.resolve('react-refresh/babel')
-                        ].filter(Boolean)
-                    }
-                },
-            },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            }
-        ]
-    }
+  /*entry -> fala onde fica o arquivo principal */
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", "tsx"],
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    hot: true,
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ].filter(Boolean),
+  /*como a aplicação vai se comportar */
+  module: {
+    rules: [
+      {
+        /*test recebe uma expressão regular pra dizer se o arquivo é js ou não */
+        test: /\.(j|t)sx$/,
+        exclude: /node_modules/,
+        /*babel-loade faz a integração do babel e webapck */
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: [
+              isDevelopment && require.resolve("react-refresh/babel"),
+            ].filter(Boolean),
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
