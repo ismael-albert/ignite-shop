828ca1d31654b297d04582f8e5b902dcbc155359
Ismael Albert <albertismael7@gmail.com>
2021-08-28 22:51:49 -0300

    .config auto

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,9 +1,9 @@
-export function Repositoryitem(props){
-    return(
-    <li>
-        <strong>{props.repositoryyy.name}</strong>
-        <p>{props.repositoryyy.description}</p>   
-        <a href={props.repositoryyy.html_url}>Acessar repositorio</a>
-    </li>
-    )
+export function Repositoryitem(props) {
+    return (
+        <li>
+            <strong>{props.repositoryyy.name}</strong>
+            <p>{props.repositoryyy.description}</p>
+            <a href={props.repositoryyy.html_url}>Acessar repositorio</a>
+        </li>
+    );
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,35 +1,27 @@
 import { useState, useEffect } from "react";
 import { Repositoryitem } from "./RepositoryItem";
-import '../styles/repositories.scss'
-
+import "../styles/repositories.scss";
 
 //https://api.github.com/users/ismael-albert/repos
 
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
 
-export function RepositoryList(){
+  useEffect(() => {
+    fetch("https://api.github.com/users/ismael-albert/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
 
-    const [repositories, setRepositories] = useState([])
-  
-    useEffect(() => {
-        fetch('https://api.github.com/users/ismael-albert/repos')
-        .then(response => response.json())
-        .then(data => setRepositories(data))
-        
-    }, []);
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorios</h1>
 
-
-
-    return(
-        <section className="repository-list">
-            <h1>Lista de repositorios</h1>
-            <ul>
-               
-            {repositories.map(repository => {
-
-                return <Repositoryitem repositoryyy={repository}/>
-
-                })}
-            </ul>
-        </section>
-    )
+      <ul>
+        {repositories.map((repository) => {
+          return <Repositoryitem repositoryyy={repository} />;
+        })}
+      </ul>
+    </section>
+  );
 }
